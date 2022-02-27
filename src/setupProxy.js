baba342e836122b83fb3d5643eae917a02d230f7
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-02-27 16:14:40 +0100

    modyfikacje

---
+++ src/setupProxy.js
@@ -0,0 +1,7 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    createProxyMiddleware(["/loginUserDatabase", , "/regestry"], { target: "http://localhost:5000" })
+  );
+};
