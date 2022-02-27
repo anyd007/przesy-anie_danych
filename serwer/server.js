a60ab3da9ca241ec632d0eb555528e64033725e0
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-02-27 20:20:53 +0100

    modyfikacje

--- serwer/server.js
+++ serwer/server.js
@@ -41,10 +41,10 @@ app.get("/regestry", (req,res)=>{
 const herokuPort = process.env.PORT || 5000
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, '/build')));
+    app.use(express.static(path.join(__dirname, 'build')));
   // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, '/build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
   }
 
