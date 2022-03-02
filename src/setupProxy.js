618c991b901e930bbccc35ed12d2ca94144f0193
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-02 16:19:59 +0100

    cors...

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api/regestry',
+    createProxyMiddleware({
+      target: 'https://serwer-dream-team.herokuapp.com',
+      changeOrigin: true,
+    })
+  );
+};
