2ae886bd6db7196856716d30801fcfefb5a43341
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-04 10:47:15 +0100

    testy

--- src/components/logowanie/Regester.jsx
+++ src/components/logowanie/Regester.jsx
@@ -7,7 +7,7 @@ const Regester = (props) => {
 
   // przesyłanie na serwer express
   const sendRegestryToBackEnd = (username, password, repassword) => {
-    fetch(`${process.env.REACT_APP_REG_API}`, {
+    fetch('https://express-heroku-nba-app.herokuapp.com/api/regestry', {
       method: "POST",
       body: JSON.stringify({
         id: uniqid(),
